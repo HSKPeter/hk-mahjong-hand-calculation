fdbb4c1a962d92dc90eee73d275b30db95bfdbb5
Peter Hui <huishikuen@gmail.com>
2021-12-12 17:01:42 -0800

    added new exportations

--- package/index.ts
+++ package/index.ts
@@ -1,14 +1,17 @@
+import Tile from "./Tile";
+import Meld from "./meld/Meld";
 import WinningHand from "./WinningHand";
 import Hand from "./Hand";
+import ExplorerOfWinningPermutations from "./depthFirstSearch/ExplorerOfWinningPermutations";
 import FaanCalculator from "./calculateFaan/FaanCalculator";
-import Meld from "./meld/Meld";
-import Tile from "./Tile";
-
+import HandTypeFinder from "./calculateFaan/HandTypeFinder";
 
 export { 
     WinningHand,
     Hand,
     FaanCalculator,
     Meld,
-    Tile
+    Tile,
+    ExplorerOfWinningPermutations,
+    HandTypeFinder
 };
