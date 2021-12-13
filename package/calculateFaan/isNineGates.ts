c1a371fdf37649b4dca7d1f10a76cec3fa868e49
Peter Hui <huishikuen@gmail.com>
2021-12-12 17:00:30 -0800

    changed the map object to array to avoid ts compiling error

--- package/calculateFaan/isNineGates.ts
+++ package/calculateFaan/isNineGates.ts
@@ -2,18 +2,21 @@ import WinningHand from "../WinningHand";
 import isAllOneSuit from "./isAllOneSuit";
 
 export default function isNineGates(inputWinningHand: WinningHand) {
-    // 111-2345678-999
-    const map = {
-        1: 3,
-        2: 1,
-        3: 1,
-        4: 1,
-        5: 1,
-        6: 1,
-        7: 1,
-        8: 1,
-        9: 3
-    }
+    
+    // const map = {
+    //     "1": 3,
+    //     "2": 1,
+    //     "3": 1,
+    //     "4": 1,
+    //     "5": 1,
+    //     "6": 1,
+    //     "7": 1,
+    //     "8": 1,
+    //     "9": 3
+    // }
+    
+    // 111-2345678-999  
+    const map = [3,1,1,1,1,1,1,1,3];
 
     // Pre-requisite of a NineGates is consisting only one type of suit.
     if (!isAllOneSuit(inputWinningHand)){
@@ -26,13 +29,13 @@ export default function isNineGates(inputWinningHand: WinningHand) {
         if (tile.getSuit() === "honor") return false;
 
         const tileValue = tile.getValue();
-        if (map[tileValue] === 0) continue;
+        if (map[tileValue - 1] === 0) continue;
         
-        map[tileValue] -- ;
+        map[tileValue - 1] -- ;
     }
 
-    for (const key in map){
-        if (map[key] !== 0) return false;
+    for (const value of map){
+        if (value !== 0) return false;
     }
 
     return true;
