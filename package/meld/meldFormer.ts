3aaa6ffb2561db2f6cbfde3b53817796ff47b39d
Peter Hui <huishikuen@gmail.com>
2021-12-12 17:05:18 -0800

    created new classes

---
+++ package/meld/meldFormer.ts
@@ -0,0 +1,16 @@
+import Tile from "../Tile";
+import Meld from "./Meld";
+
+/**
+ * This is a light-weighted function that takes an array if Unicode strings (e.g. ["🀀", "🀀", "🀀"]) to form a Meld.
+ * @param inputTilesStrings 
+ * @returns meldFormed as Meld
+ */
+export default function meldFormer(inputTilesStrings: string[]): Meld{
+    const tiles: Tile[] = [];
+    for (const tile of inputTilesStrings){
+        tiles.push(new Tile(tile));
+    }
+    const meldFormed = new Meld(tiles);
+    return meldFormed;
+}
