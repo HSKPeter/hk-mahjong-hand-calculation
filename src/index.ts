c6453d1ac0b936352c6ed2b17d094ae7022cb7f1
Peter Hui <huishikuen@gmail.com>
2021-12-22 18:48:30 -0800

    imported and exported meldFormer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Tile from './tile/Tile';
 import Meld from './meld/Meld';
+import meldFormer from './meld/meldFormer';
 import WinningHand from './hand/WinningHand';
 import Hand from './hand/Hand';
 import ExplorerOfWinningPermutations from './depthFirstSearch/ExplorerOfWinningPermutations';
 import FaanCalculator from './calculateFaan/FaanCalculator';
 import HandTypeFinder from './hand/handType/HandTypeFinder';
 
-export { WinningHand, Hand, FaanCalculator, Meld, Tile, ExplorerOfWinningPermutations, HandTypeFinder };
+
+export { WinningHand, Hand, FaanCalculator, Meld, meldFormer, Tile, ExplorerOfWinningPermutations, HandTypeFinder };
