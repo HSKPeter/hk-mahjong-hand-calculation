9dcf1dfd733d1c047dc9431c82f274b8e8b1b03b
Peter Hui <huishikuen@gmail.com>
2021-12-22 18:48:14 -0800

    created new test cases

---
+++ src/__tests__/meldFormer.test.ts
@@ -0,0 +1,9 @@
+import { meldFormer, Meld, Tile } from '../index';
+
+test('Compare two melds that are formed in different ways', () => {
+    const meld1 = meldFormer(["🀀", "🀀", "🀀"]);
+
+    const east = new Tile({ suit: "honor", value: 1 })
+    const meld2 = new Meld([east, east, east]);
+    expect(meld1.isIdentical(meld2)).toBe(true);
+});
